aaa61eadc8d69056f4175697021f529e9b7f08af
Heesung jang <heesungj7@gmail.com>
2021-09-16 03:41:40 +0900

    [feat] 타입스크립트 매개변수

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello, world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nicolas";
+const age = 24;
+const gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, You are ${age}. you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello, world");
+const name = "Nicolas";
+const age = 24;
+const gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, You are ${age}. you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
