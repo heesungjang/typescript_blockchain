9b19c1973e07d8db185c3870a3ff8d785a6db814
Heesung jang <heesungj7@gmail.com>
2021-09-16 04:07:43 +0900

    클래스 타입 정의 + private / public

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "heesung",
-    gender: "male",
-    age: 22,
-};
+const heesung = new Human("heesung", 18, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, You are ${person.age}. you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
-
+console.log(sayHi(heesung));
 export {};
