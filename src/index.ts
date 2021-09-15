9fe78bc97bd3fea3d55a089542e79febf661fc4c
Heesung jang <heesungj7@gmail.com>
2021-09-16 04:15:56 +0900

    첫 블럭 생성

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-class Human {
-    public name: string;
-    private age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const heesung = new Human("heesung", 18, "female");
+const genesisBlock: Block = new Block(0, "213123123", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, You are ${person.age}. you are a ${person.gender}!`;
-};
+let blockChain: [Block] = [genesisBlock];
 
-console.log(sayHi(heesung));
+console.log(blockChain);
+
 export {};
